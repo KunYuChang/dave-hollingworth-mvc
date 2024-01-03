86676d19b3c9aa6c1e1c869d883c373db7d7c461
KunYuChang <mischiefsub@gmail.com>
2024-01-04 04:23:34 +0800

    使用純 PHP 從資料庫讀取資料

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=product_db;charset=utf8;port=3306";
+
+$pdo = new PDO($dsn, "root", "", [
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
+
+$stmt = $pdo->query("SELECT * FROM product");
+
+$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+print_r($products);
