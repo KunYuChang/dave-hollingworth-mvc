25928923ec423a8715e950adc686aaa13117bcd3
KunYuChang <mischiefsub@gmail.com>
2024-01-04 06:32:15 +0800

    將路由路徑與路由表中的路由進行匹配

--- index.php
+++ index.php
@@ -7,6 +7,10 @@ $router = new Router;
 $router->add("/home/index", ["controller" => "home", "action" => "index"]);
 $router->add("/products", ["controller" => "products", "action" => "index"]);
 $router->add("/", ["controller" => "home", "action" => "index"]);
+$params = $router->match($path);
+
+var_dump($params);
+exit;
 
 $segments = explode('/', $path);
 $action = $segments[2];
