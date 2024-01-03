37e1cfbcb3059ef85e647f8f2cd0587fc2fc3963
KunYuChang <mischiefsub@gmail.com>
2024-01-04 04:50:50 +0800

    調整純 PHP 程式碼以適應 MVC 模式：建立 View

--- index.php
+++ index.php
@@ -3,24 +3,4 @@ require "model.php";
 $model = new Model;
 $products = $model->getData();
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-	<meta charset="UTF-8">
-	<title>Products</title>
-</head>
-
-<body>
-
-	<h1>Products</h1>
-
-	<?php foreach ($products as $product) : ?>
-		<h2><?= htmlspecialchars($product['name']) ?></h2>
-		<p><?= htmlspecialchars($product['description']) ?></p>
-	<?php endforeach; ?>
-
-</body>
-
-</html>
+require "view.php";
