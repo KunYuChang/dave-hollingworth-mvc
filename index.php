c829ce3ef19564d4619fdfc32bbde6ca0231c857
KunYuChang <mischiefsub@gmail.com>
2024-01-04 05:37:34 +0800

    透過動態建立物件和運行方法來消除重複

--- index.php
+++ index.php
@@ -3,16 +3,6 @@
 $action = $_GET['action'];
 $controller = $_GET['controller'];
 
-if ($controller === 'products') {
-	require "src/controllers/products.php";
-	$controller_object = new Products;
-} elseif ($controller === 'home') {
-	require "src/controllers/home.php";
-	$controller_object = new Home;
-}
-
-if ($action === 'index') {
-	$controller_object->index();
-} elseif ($action === 'show') {
-	$controller_object->show();
-}
+require "src/controllers/$controller.php";
+$controller_object = new $controller;
+$controller_object->$action();
