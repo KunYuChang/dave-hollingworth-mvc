a2dcbad5f94f7afefc49b039fb3d6e3d65165e7c
KunYuChang <mischiefsub@gmail.com>
2024-01-04 04:47:23 +0800

    調整純 PHP 程式碼以適應 MVC 模式：建立 Model 類別

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
-
-$dsn = "mysql:host=localhost;dbname=product_db;charset=utf8;port=3306";
-
-$pdo = new PDO($dsn, "root", "", [
-	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-]);
-
-$stmt = $pdo->query("SELECT * FROM product");
-
-$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+require "model.php";
+$model = new Model;
+$products = $model->getData();
 
 ?>
 <!DOCTYPE html>
