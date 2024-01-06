7c2e18d3ffd617c81e3feacb271625cb91846f00
KunYuChang <mischiefsub@gmail.com>
2024-01-06 19:15:54 +0800

    從URL取得值

--- index.php
+++ index.php
@@ -7,9 +7,11 @@ spl_autoload_register(function (string $class_name) {
 });
 
 $router = new Framework\Router;
-$router->add("/home/index", ["controller" => "home", "action" => "index"]);
-$router->add("/products", ["controller" => "products", "action" => "index"]);
-$router->add("/", ["controller" => "home", "action" => "index"]);
+$router->add("/{controller}/{action}");
+$router->add("/{controller}/{id:\d+}/{action}");
+// $router->add("/home/index", ["controller" => "home", "action" => "index"]);
+// $router->add("/products", ["controller" => "products", "action" => "index"]);
+// $router->add("/", ["controller" => "home", "action" => "index"]);
 $params = $router->match($path);
 
 if ($params === false) {
