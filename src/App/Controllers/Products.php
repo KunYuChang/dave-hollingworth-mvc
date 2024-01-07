43938170e30f56a7c4aa46a1bf9f999a0ee62b8b
KunYuChang <mischiefsub@gmail.com>
2024-01-08 06:19:48 +0800

    Dependency Injection (DI) and Autowiring

--- src/App/Controllers/Products.php
+++ src/App/Controllers/Products.php
@@ -7,31 +7,32 @@ use Framework\Viewer;
 
 class Products
 {
+	public function __construct(
+		private Viewer $viewer,
+		private Product $model
+	) {
+	}
+
 	public function index()
 	{
-		$model = new Product;
-		$products = $model->getData();
+		$products = $this->model->getData();
 
-		$viewer = new Viewer;
-
-		echo $viewer->render("shared/header.php", [
+		echo $this->viewer->render("shared/header.php", [
 			"title" => "Products"
 		]);
 
-		echo $viewer->render("Products/index.php", [
+		echo $this->viewer->render("Products/index.php", [
 			"products" => $products
 		]);
 	}
 
 	public function show(string $id)
 	{
-		$viewer = new Viewer;
-
-		echo $viewer->render("shared/header.php", [
+		echo $this->viewer->render("shared/header.php", [
 			"title" => "Product"
 		]);
 
-		echo $viewer->render("Products/show.php", [
+		echo $this->viewer->render("Products/show.php", [
 			"id" => $id
 		]);
 	}
